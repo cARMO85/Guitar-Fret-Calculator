8a785427eb46b8815d23cfb5091b6ed407fdd8fe
Paul Carmody <paul.a.e.carmody@gmail.com>
2023-10-04 09:09:11 +0200

    Updated template file location

--- fret_calculator.py
+++ fret_calculator.py
@@ -1,10 +1,7 @@
 from flask import Flask, render_template, request
-app = Flask(__name__, template_folder='/Users/paul/Documents/Project _work/Fret Calculator/Templates')
 import os
 
-
-
-
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
